6aabbb42a928619db787c6918b135f43287624e7
XenorInspire <nicolas.fouchard94370@gmail.com>
2020-11-09 10:54:28 +0100

    HOTFIX last command

--- installVirtualbox.sh
+++ installVirtualbox.sh
@@ -5,7 +5,7 @@
 
 echo "This script requires root privileges"
 yum update -y
-yum autoremove
+yum autoremove -y
 
 yum install -y kernel-devel
 yum install -y kernel-headers
@@ -15,7 +15,6 @@ yum install -y perl
 yum install -y elfutils-libelf-devel
 yum install -y ./VirtualBox-6.1-6.1.16_140961_el8-1.x86_64.rpm
 
-echo "This operation may take a few minutes"
-/usr/sbin/vboxconfig
-
-echo "Installation finished"
+echo "You must reboot now to continue the installation"
+echo "After this, enter the command : '/usr/sbin/vboxconfig' to finish it"
+#/usr/sbin/vboxconfig
